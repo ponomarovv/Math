667a32c45424b3e503f823c53f50371ce111334c
Ponomarov Oleksii <testdrive819@gmail.com>
2023-03-24 01:00:41 +0200

    add reversemap and remove comments

--- Math.BLL.Impl/Mappers/MappersProfile.cs
+++ Math.BLL.Impl/Mappers/MappersProfile.cs
@@ -8,30 +8,16 @@ public class MappersProfile : AutoMapper.Profile
     public MappersProfile()
     {
         CreateMap<Answer, AnswerModel>()
-            .ForMember(dest => dest.QuestionModel, 
-                opt => opt.MapFrom(src => src.Question))
-            // .IncludeMembers(a=>a.Question)
-            // .IncludeMembers(src=> src.Question.Answers).ReverseMap()
-            ;
-            
-        // CreateMap<Question, QuestionModel>().ReverseMap()
-        //     .ForMember(dest => dest.Answers, opt => opt.MapFrom(src => src.AnswerModels))
-        //     .ForMember(dest => dest.Topic, opt => opt.MapFrom(src => src.TopicModel))
-        //     ;    
-        
+            .ForMember(dest => dest.QuestionModel, opt => opt.MapFrom(src => src.Question))
+            .ReverseMap();
+
         CreateMap<Question, QuestionModel>()
             .ForMember(dest => dest.AnswerModels, opt => opt.MapFrom(src => src.Answers))
             .ForMember(dest => dest.TopicModel, opt => opt.MapFrom(src => src.Topic))
-            // .IncludeMembers(q=>q.Answers)
-            // .IncludeMembers(q=>q.Topic)     
-     
-            ;
-        
+            .ReverseMap();
+
         CreateMap<Topic, TopicModel>()
             .ForMember(dest => dest.QuestionModels, opt => opt.MapFrom(src => src.Questions))
-            // .IncludeMembers(t=>t.Questions)
-            ;
-        
-
+            .ReverseMap();
     }
 }
