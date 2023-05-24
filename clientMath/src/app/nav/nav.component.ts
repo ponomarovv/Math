1d4bac9b8c3cf7c4f46ca64e62289aa04c3503ca
Ponomarov Oleksii <testdrive819@gmail.com>
2023-05-25 02:43:18 +0300

    burger fixed

--- clientMath/src/app/nav/nav.component.ts
+++ clientMath/src/app/nav/nav.component.ts
@@ -1,9 +1,8 @@
-import {Component, OnInit, HostListener} from '@angular/core';
-
-import {Router} from "@angular/router";
-import {ToastrService} from "ngx-toastr";
-import {UserService} from "../_services/user.service";
-import {SharedService} from "../_services/shared.service";
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../_services/user.service';
+import { SharedService } from '../_services/shared.service';
 
 @Component({
   selector: 'app-nav',
@@ -11,15 +10,18 @@ import {SharedService} from "../_services/shared.service";
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
   isLoggedIn: any;
-  fullName: any = '';
+  fullName: string = '';
 
-  userId: string='';
+  isBurgerMenuOpen: boolean = false;
+  isProfileButtonsVisible: boolean = true;
 
-  constructor(private router: Router, private toastr: ToastrService, private userService: UserService, private sharedService: SharedService) {
-  }
-
+  constructor(
+    private router: Router,
+    private toastr: ToastrService,
+    private userService: UserService,
+    private sharedService: SharedService
+  ) {}
 
   ngOnInit(): void {
     this.sharedService.fullName$.subscribe(fullName => {
@@ -35,26 +37,36 @@ export class NavComponent implements OnInit {
     });
 
     this.userService.fullName$.subscribe(fullName => {
-      this.fullName = fullName});
-
-    this.userService.userId$.subscribe(userId => {
-      this.userId = userId;
+      this.fullName = fullName;
     });
-
   }
 
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: Event) {
+    this.updateButtonVisibility();
+  }
+
+  toggleBurgerMenu() {
+    this.isBurgerMenuOpen = !this.isBurgerMenuOpen;
+    this.updateButtonVisibility();
+  }
+
+  private updateButtonVisibility() {
+    const windowWidth = window.innerWidth;
+    if (windowWidth <= 768) {
+      this.isProfileButtonsVisible = !this.isBurgerMenuOpen;
+    } else {
+      this.isProfileButtonsVisible = true;
+    }
+  }
 
   SignOut() {
     localStorage.clear();
     this.router.navigate(['/user/login']);
-
     this.isLoggedIn = null;
     console.log('logged out');
-
-    this.userService.setLoggedIn(false)
+    this.userService.setLoggedIn(false);
     this.toastr.success('Logout successful', 'Logout');
-
     this.fullName = 'Guest';
   }
-
 }
