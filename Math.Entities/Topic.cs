39f002223b1eea9b13ecfad1e326c71c4c96de41
Ponomarov Oleksii <testdrive819@gmail.com>
2023-05-31 01:34:48 +0300

    add topicId to topic entity for recurrent db requests. and to build math topics tree

--- Math.Entities/Topic.cs
+++ Math.Entities/Topic.cs
@@ -3,6 +3,7 @@
 public class Topic
 {
     public int Id { get; set; }
+    public int TopicId { get; set; }
     public string? Text { get; set; }
     
     public virtual ICollection<Question> Questions { get; set; }
--- Math.Models/TopicModel.cs
+++ Math.Models/TopicModel.cs
@@ -3,6 +3,7 @@
 public class TopicModel
 {
     public int Id { get; set; }
+    public int TopicId { get; set; }
     public string? Text { get; set; }
     public virtual ICollection<QuestionModel> QuestionModels { get; set; }
 }
