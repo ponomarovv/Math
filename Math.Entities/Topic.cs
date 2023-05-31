3e48c9db48db9f8268bfa1c69d751c0dbb3f3a2b
Ponomarov Oleksii <testdrive819@gmail.com>
2023-06-01 00:29:39 +0300

    topic entity fixed for math disciplines tree

--- Math.Entities/Topic.cs
+++ Math.Entities/Topic.cs
@@ -3,11 +3,12 @@
 public class Topic
 {
     public int Id { get; set; }
-    public int TopicId { get; set; }
+    
     public string? Text { get; set; }
     
     public virtual ICollection<Question> Questions { get; set; }
 
     public virtual ICollection<Quiz> Quizzes { get; set; }
     public virtual ICollection<Book> Books { get; set; }
+    public Topic AnotherTopic { get; set; }
 }
