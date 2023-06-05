b352e3c9ba6da8ead299549ad8d878f11d2391fb
Ponomarov Oleksii <testdrive819@gmail.com>
2023-06-05 23:01:36 +0300

    fix. remove virtual lazy loading.

--- Math.Entities/TopicEntity/Topic.cs
+++ Math.Entities/TopicEntity/Topic.cs
@@ -6,14 +6,13 @@ public class Topic
 
     public string? Text { get; set; }
 
-    public virtual ICollection<Question> Questions { get; set; }
+    public ICollection<Question> Questions { get; set; }
 
-    public virtual ICollection<Quiz> Quizzes { get; set; }
-    public virtual ICollection<Book> Books { get; set; }
+    public ICollection<Quiz> Quizzes { get; set; }
+    public ICollection<Book> Books { get; set; }
 
-   
+
     public int? ParentTopicId { get; set; }
-    public virtual Topic ParentTopic { get; set; }
-    public virtual ICollection<Topic> ChildTopics { get; set; }
-
+    public Topic ParentTopic { get; set; }
+    public ICollection<Topic> ChildTopics { get; set; }
 }
--- Math.Models/TopicModel/TopicModel.cs
+++ Math.Models/TopicModel/TopicModel.cs
@@ -3,13 +3,13 @@
 public class TopicModel
 {
     public int Id { get; set; }
-   
+
     public string? Text { get; set; }
-    public virtual ICollection<QuestionModel> QuestionModels { get; set; }
-    public virtual ICollection<QuizModel> QuizModels { get; set; }
-    public virtual ICollection<BookModel> BookModels { get; set; }
-    
+    public ICollection<QuestionModel> QuestionModels { get; set; }
+    public ICollection<QuizModel> QuizModels { get; set; }
+    public ICollection<BookModel> BookModels { get; set; }
+
     public int? ParentTopicId { get; set; }
-    public virtual TopicModel ParentTopic { get; set; }
-    public virtual ICollection<TopicModel> ChildTopics { get; set; } 
+    public TopicModel ParentTopic { get; set; }
+    public ICollection<TopicModel> ChildTopics { get; set; }
 }
