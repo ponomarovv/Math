3034021a0e080cf3471ab3ef05b26d5cd481c846
Ponomarov Oleksii <testdrive819@gmail.com>
2023-06-13 02:45:35 +0300

    fix M:N now for books and topics

--- Math.Entities/Book.cs
+++ Math.Entities/Book.cs
@@ -9,5 +9,5 @@ public class Book
     public int Id { get; set; }
     public string Text { get; set; }
 
-    public Topic Topic { get; set; }
+    public ICollection<Topic> Topics { get; set; }
 }
